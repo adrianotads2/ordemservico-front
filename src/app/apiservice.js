53a8a416af34cf4f4dc6f232c11d69729137584c
adrianotads2 <altairmt@gmail.com>
2021-12-04 17:17:48 -0400

    refatoracão

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
-   //baseURL: 'http://adr-ordemservico.herokuapp.com'
+    //baseURL: 'http://localhost:8080'
+   baseURL: 'http://adr-ordemservico.herokuapp.com'
 });
 
 class ApiService {
